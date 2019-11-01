3b4af3b6cb0c04cc603986f45b11038ed349124a
jeremyottley <jeremy.ottley@gmail.com>
2019-10-31 21:14:05 -0400

    continued

---
+++ read-file.scm
@@ -0,0 +1,20 @@
+#!/usr/bin/csi -ss
+
+;; An example file reading script with chicken scheme
+
+(use extras)
+
+(define (read-it file)
+  (let ((fh (open-input-file file)))
+       (let loop((c (read-line fh)))
+               (if (eof-object? c)
+                    (close-input-port fh)
+                          (begin
+                                   (print c)
+                                            (loop (read-line fh)))))))
+
+                                            (define (main args)
+                                              (define file (if (null? args) '() (car args)))
+                                                (if (null? file)
+                                                    (display "Please supply a file name to read!\n")
+                                                        (read-it file)))
