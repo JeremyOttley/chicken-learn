c48acc47038992044dcdfe7b8f4df731f855ea3c
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-04 09:25:26 -0500

    Update read-file.scm

--- read-file.scm
+++ read-file.scm
@@ -5,16 +5,16 @@
 (use extras)
 
 (define (read-it file)
-  (let ((fh (open-input-file file)))
-       (let loop((c (read-line fh)))
-               (if (eof-object? c)
-                    (close-input-port fh)
-                          (begin
-                                   (print c)
-                                            (loop (read-line fh)))))))
+    (let ((fh (open-input-file file)))
+        (let loop((c (read-line fh)))
+            (if (eof-object? c)
+                (close-input-port fh)
+                    (begin
+                        (print c)
+                            (loop (read-line fh)))))))
 
-                                            (define (main args)
-                                              (define file (if (null? args) '() (car args)))
-                                                (if (null? file)
-                                                    (display "Please supply a file name to read!\n")
-                                                        (read-it file)))
+(define (main args)
+    (define file (if (null? args) '() (car args)))
+        (if (null? file)
+            (display "Please supply a file name to read!\n")
+                (read-it file)))
